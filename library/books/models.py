1bda5396a3ab0f6d8a46b54995afb62efc34ab9b
Zeynab Amini <z_amini@arsh.co>
2021-08-04 18:08:29 +0430

    chore: add borrow logics to books models

--- library/books/models.py
+++ library/books/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 from django.core.validators import MinLengthValidator
-from django.db import models
+from django.db import models, transaction
+from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
 
@@ -46,6 +48,16 @@ class Book(models.Model):
     def __str__(self):
         return self.title
 
+    @property
+    def is_available(self):
+        out_copies = self.borrow_set.select_for_update().filter(
+            returned_at__isnull=True
+        )
+        return out_copies.count() < self.copies
+
+    def related_books(self):
+        return Book.objects.filter(type=self.type, tags__in=self.tags)
+
 
 class Borrow(models.Model):
     student = models.ForeignKey(
@@ -72,6 +84,44 @@ class Borrow(models.Model):
 
     def __str__(self):
         return f"{self.student.get_full_name()}: {self.book}"
+
+    @property
+    def out_days(self):
+        if not self.borrowed_at:
+            return 0
+        ending = self.returned_at or timezone.now()
+        return (ending.date() - self.borrowed_at.date()).days + 1
+
+    @property
+    def is_overdue(self):
+        if not self.borrowed_at:
+            return False
+        return self.out_days > self.duration.days
+
+    def clean_student(self):
+        already_borrowed = self.student.borrow_set.filter(returned_at__isnull=True)
+        if already_borrowed.exists():
+            raise ValidationError(_("You have not returned previously borrowed book."))
+        unpaid_penalties = DelayPenalty.objects.filter(
+            borrow__student=self.student, is_paid=False
+        )
+        if unpaid_penalties.exists():
+            raise ValidationError(_("You has an unpaid delay penalty."))
+
+    def clean_book(self):
+        if not self.book.is_available:
+            raise ValidationError(_("No copy of this book is available right now."))
+
+    @transaction.atomic
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.clean_student()
+            self.clean_book()
+        super(Borrow, self).save(*args, **kwargs)
+        if self.is_overdue:
+            DelayPenalty.objects.get_or_create(
+                borrow=self, defaults={"amount": self.out_days * 1000}
+            )
 
 
 class DelayPenalty(models.Model):
